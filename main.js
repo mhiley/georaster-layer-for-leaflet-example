fae7299eeb85236ab4b8b1f6822d391fd15cbb15
Michael Hiley <hiley@earthlinginteractive.com>
2019-01-09 15:13:29 -0600

    snapshot, send url to parse_georaster instead of data

--- main.js
+++ main.js
@@ -10,29 +10,54 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var url_to_geotiff_file = "example_4326.tif";
+// var url_to_geotiff_file = "example_4326.tif";
+// var url_to_geotiff_file = "LC08_L1TP_024030_20180723_20180731_01_T1_B1_4326.TIF";
+var url_to_geotiff_file = "http://localhost:8080/LC08_L1TP_024030_20180723_20180731_01_T1_B1_4326.TIF";
 
-fetch(url_to_geotiff_file)
-  .then(response => response.arrayBuffer())
-  .then(arrayBuffer => {
-    parse_georaster(arrayBuffer).then(georaster => {
-      console.log("georaster:", georaster);
+//fetch(url_to_geotiff_file)
+//  .then(response => response.arrayBuffer())
+//  .then(arrayBuffer => {
+//    parse_georaster(arrayBuffer).then(georaster => {
+//      console.log("georaster:", georaster);
+//
+//      /*
+//          GeoRasterLayer is an extension of GridLayer,
+//          which means can use GridLayer options like opacity.
+//
+//          Just make sure to include the georaster option!
+//
+//          http://leafletjs.com/reference-1.2.0.html#gridlayer
+//      */
+//      var layer = new GeoRasterLayer({
+//          georaster: georaster,
+//          opacity: 1.0,
+//          resolution: Math.pow(2, 7)
+//      });
+//      layer.addTo(map);
+//
+//      map.fitBounds(layer.getBounds());
+//
+//  });
+//});
 
-      /*
-          GeoRasterLayer is an extension of GridLayer,
-          which means can use GridLayer options like opacity.
+parse_georaster(url_to_geotiff_file, null, true).then(georaster => {
+  console.log("georaster:", georaster);
 
-          Just make sure to include the georaster option!
+  /*
+      GeoRasterLayer is an extension of GridLayer,
+      which means can use GridLayer options like opacity.
 
-          http://leafletjs.com/reference-1.2.0.html#gridlayer
-      */
-      var layer = new GeoRasterLayer({
-          georaster: georaster,
-          opacity: 0.7
-      });
-      layer.addTo(map);
+      Just make sure to include the georaster option!
 
-      map.fitBounds(layer.getBounds());
-
+      http://leafletjs.com/reference-1.2.0.html#gridlayer
+  */
+  var layer = new GeoRasterLayer({
+      georaster: georaster,
+      opacity: 1.0,
+      resolution: Math.pow(2, 7)
   });
+  layer.addTo(map);
+
+  map.fitBounds(layer.getBounds());
+
 });
